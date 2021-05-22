d09c13b02ee050d2c835d255e5bc2eabc4b2dc91
Ram Pukar <ram_mosone@yahoo.com>
2021-05-22 17:30:20 +0545

    ChartJS Seperated

--- resources/views/chart/index.blade.php
+++ resources/views/chart/index.blade.php
@@ -46,5 +46,75 @@
 @endsection
 
 @push('scripts')
-    <script src="{{ asset('/js/pages/dashboard3.js') }}"></script>
+<script>
+    $(function () {
+        'use strict'
+        var ticksStyle = {
+            fontColor: '#495057',
+            fontStyle: 'bold'
+        }
+        var mode = 'index'
+        var intersect = true
+
+        // Report Dashboard
+        var $reportChart = $('#report-chart')
+        var reportChart = new Chart($reportChart, {
+            type: 'bar',
+            data: {
+            labels: ['JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'],
+            datasets: [
+                {
+                backgroundColor: '#007bff',
+                borderColor: '#007bff',
+                data: [1000, 2000, 3000, 2500, 2700, 2500, 3000]
+                }
+            ]
+            },
+            options: {
+            maintainAspectRatio: false,
+            tooltips: {
+                mode: mode,
+                intersect: intersect
+            },
+            hover: {
+                mode: mode,
+                intersect: intersect
+            },
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                // display: false,
+                gridLines: {
+                    display: true,
+                    lineWidth: '4px',
+                    color: 'rgba(0, 0, 0, .2)',
+                    zeroLineColor: 'transparent'
+                },
+                ticks: $.extend({
+                    beginAtZero: true,
+
+                    // Include a dollar sign in the ticks
+                    callback: function (value, index, values) {
+                    if (value >= 1000) {
+                        value /= 1000
+                        value += 'k'
+                    }
+                    return '$' + value
+                    }
+                }, ticksStyle)
+                }],
+                xAxes: [{
+                display: true,
+                gridLines: {
+                    display: false
+                },
+                ticks: ticksStyle
+                }]
+            }
+            }
+        })
+    })
+</script>
 @endpush
